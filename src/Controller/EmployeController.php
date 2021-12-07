a32178b6ae8db34be4b5293686f64eb0c10ed8cc
Kerebel <bastienkerebel@gmail.com>
2021-12-07 09:57:44 +0100

    TP achevé

--- src/Controller/EmployeController.php
+++ src/Controller/EmployeController.php
@@ -45,4 +45,21 @@ class EmployeController extends AbstractController
             'nom'=>$nom
         );
     }
+    
+    /**
+     * @Route(path= "employe/{nom}",name="employe_redirection", requirements = {"nom":"[A-Za-z]+"})
+     */
+    public function redirection(string $nom) {
+        $nom = "Bond";
+        $url = $this->generateUrl("employe_employe_voirnomB", array('nom' => $nom));
+        return $this->redirect($url);
+    }
+    
+    /**
+     * @Route(path= "employe2/{nom}", name="employe2_redirection", requirements = {"nom":"[A-Za-z]+"})
+     */
+    public function redirectionV2(string $nom){
+        $nom = "Bond";
+        return $this->redirectToRoute("employe_employe_voirnomB", ['nom' => $nom,]);
+    }
 }
